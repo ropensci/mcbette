5b767d544f43d8a7fa1d87f2cb3695bc37d1e0f2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-24 18:39:24 +0100

    Run all site models

--- run.R
+++ run.R
@@ -9,12 +9,12 @@ testit::assert(file.exists(fasta_filename))
 print("site_model_name | marg_log_lik | marg_log_lik_sd")
 
 # Pick a site model
-site_model <- beautier:::create_site_models()[1]
-marg_lik <- bbt_run(
-  fasta_filenames = fasta_filename,
-  site_models = site_model,
-  mcmc = create_mcmc_nested_sampling(),
-  beast2_path = get_default_beast2_bin_path()
-)$ns
-
-print(paste(site_model[[1]]$name, "|",marg_lik$marg_log_lik, "|", marg_lik$marg_log_lik_sd))
+for (site_model in beautier:::create_site_models()) {
+  marg_lik <- bbt_run(
+    fasta_filenames = fasta_filename,
+    site_models = site_model,
+    mcmc = create_mcmc_nested_sampling(),
+    beast2_path = get_default_beast2_bin_path()
+  )$ns
+  print(paste(site_model[[1]]$name, "|",marg_lik$marg_log_lik, "|", marg_lik$marg_log_lik_sd))
+}
