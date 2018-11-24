03d95b8756f374614df9bd6f466668bd84025960
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-24 19:21:17 +0100

    Show best model

--- run.R
+++ run.R
@@ -6,9 +6,13 @@ testit::assert(mauricer::mrc_is_installed("NS"))
 fasta_filename <- "primates.fas"
 testit::assert(file.exists(fasta_filename))
 
-print("site_model_name | marg_log_lik | marg_log_lik_sd")
+n_rows <- length(beautier:::create_site_models())
+site_model_names <- rep(NA, n_rows)
+marg_log_liks <- rep(NA, n_rows)
+marg_log_lik_sds <- rep(NA, n_rows)
 
 # Pick a site model
+row_index <- 1
 for (site_model in beautier:::create_site_models()) {
   marg_lik <- bbt_run(
     fasta_filenames = fasta_filename,
@@ -16,11 +20,25 @@ for (site_model in beautier:::create_site_models()) {
     mcmc = create_mcmc_nested_sampling(),
     beast2_path = get_default_beast2_bin_path()
   )$ns
-  print(
-    paste(
-      site_model$name, "|", 
-      marg_lik$marg_log_lik, "|", 
-      marg_lik$marg_log_lik_sd
-    )
-  )
+  site_model_names[row_index] <- site_model$name
+  marg_log_liks[row_index] <- marg_lik$marg_log_lik
+  marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
+  row_index <- row_index + 1
 }
+
+df <- data.frame(
+  site_model_name = site_model_names,
+  marg_log_lik = marg_log_liks,
+  marg_log_lik_sd = marg_log_lik_sds
+)
+knitr::kable(df)
+
+best_row_index <- which(df$marg_log_lik == max(df$marg_log_lik))
+
+df_best <- data.frame(
+  site_model_name = site_model_names[best_row_index],
+  marg_log_lik = marg_log_liks[best_row_index],
+  marg_log_lik_sd = marg_log_lik_sds[best_row_index]
+)
+print("Best model:")
+knitr::kable(df_best)
