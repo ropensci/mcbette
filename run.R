03709203d8a2dc16a1b9c46ebf693e331784fb59
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-24 19:29:47 +0100

    Also try clock models

--- run.R
+++ run.R
@@ -8,26 +8,32 @@ testit::assert(file.exists(fasta_filename))
 
 n_rows <- length(beautier:::create_site_models())
 site_model_names <- rep(NA, n_rows)
+clock_model_names <- rep(NA, n_rows)
 marg_log_liks <- rep(NA, n_rows)
 marg_log_lik_sds <- rep(NA, n_rows)
 
 # Pick a site model
 row_index <- 1
 for (site_model in beautier:::create_site_models()) {
-  marg_lik <- bbt_run(
-    fasta_filenames = fasta_filename,
-    site_models = site_model,
-    mcmc = create_mcmc_nested_sampling(),
-    beast2_path = get_default_beast2_bin_path()
-  )$ns
-  site_model_names[row_index] <- site_model$name
-  marg_log_liks[row_index] <- marg_lik$marg_log_lik
-  marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
-  row_index <- row_index + 1
+  for (clock_model in beautier:::create_clock_models()) {
+    marg_lik <- bbt_run(
+      fasta_filenames = fasta_filename,
+      site_models = site_model,
+      clock_models = clock_model,
+      mcmc = create_mcmc_nested_sampling(),
+      beast2_path = get_default_beast2_bin_path()
+    )$ns
+    site_model_names[row_index] <- site_model$name
+    clock_model_names[row_index] <- clock_model$name
+    marg_log_liks[row_index] <- marg_lik$marg_log_lik
+    marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
+    row_index <- row_index + 1
+  }
 }
 
 df <- data.frame(
   site_model_name = site_model_names,
+  clock_model_name = clock_model_names,
   marg_log_lik = marg_log_liks,
   marg_log_lik_sd = marg_log_lik_sds
 )
@@ -37,6 +43,7 @@ best_row_index <- which(df$marg_log_lik == max(df$marg_log_lik))
 
 df_best <- data.frame(
   site_model_name = site_model_names[best_row_index],
+  clock_model_name = clock_model_names[best_row_index],
   marg_log_lik = marg_log_liks[best_row_index],
   marg_log_lik_sd = marg_log_lik_sds[best_row_index]
 )
