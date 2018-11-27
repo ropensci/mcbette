9a1fe3409cbbb50d2c6655017b267b1a366ee37b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-27 08:36:46 +0100

    Handle errors

--- run.R
+++ run.R
@@ -18,19 +18,24 @@ row_index <- 1
 for (site_model in beautier:::create_site_models()) {
   for (clock_model in beautier:::create_clock_models()) {
     for (tree_prior in beautier:::create_tree_priors()) {
-      marg_lik <- bbt_run(
-        fasta_filenames = fasta_filename,
-        site_models = site_model,
-        clock_models = clock_model,
-        tree_priors = tree_prior,
-        mcmc = create_mcmc_nested_sampling(),
-        beast2_path = get_default_beast2_bin_path()
-      )$ns
+      tryCatch(
+        {
+          marg_lik <- bbt_run(
+            fasta_filenames = fasta_filename,
+            site_models = site_model,
+            clock_models = clock_model,
+            tree_priors = tree_prior,
+            mcmc = create_mcmc_nested_sampling(),
+            beast2_path = get_default_beast2_bin_path()
+          )$ns
+          marg_log_liks[row_index] <- marg_lik$marg_log_lik
+          marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
+        }, 
+        error = function(msg) {}
+      ) 
       site_model_names[row_index] <- site_model$name
       clock_model_names[row_index] <- clock_model$name
       tree_prior_names[row_index] <- tree_prior$name
-      marg_log_liks[row_index] <- marg_lik$marg_log_lik
-      marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
       row_index <- row_index + 1
     }
   }
