fb3890a6da9d56824a7bfbb42134e89f82ebfeb7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-28 15:15:31 +0100

    Thanks @lintr-bot

--- R/est_marg_liks.R
+++ R/est_marg_liks.R
@@ -26,8 +26,7 @@ est_marg_liks <- function(fasta_filename) {
   for (site_model in beautier:::create_site_models()) {
     for (clock_model in beautier:::create_clock_models()) {
       for (tree_prior in beautier:::create_tree_priors()) {
-        tryCatch(
-          {
+        tryCatch({
             marg_lik <- babette::bbt_run(
               fasta_filenames = fasta_filename,
               site_models = site_model,
@@ -39,7 +38,7 @@ est_marg_liks <- function(fasta_filename) {
             marg_log_liks[row_index] <- marg_lik$marg_log_lik
             marg_log_lik_sds[row_index] <- marg_lik$marg_log_lik_sd
           },
-          error = function(msg) {}
+          error = function(msg) {} # nolint indeed nothing happens here
         )
         site_model_names[row_index] <- site_model$name
         clock_model_names[row_index] <- clock_model$name
