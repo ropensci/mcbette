01c50505aaa13510a9bfe15a55535c66f4170fc8
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-14 11:03:15 +0200

    'interpret_bayes_factor' is exported

--- R/interpret_bayes_factor.R
+++ R/interpret_bayes_factor.R
@@ -1,7 +1,16 @@
-#' 
-#' From [1] H. Jeffreys (1961). The Theory of Probability (3rd ed.). Oxford. p. 432
+#' Interpret a Bayes factor
+#'
+#' Interpretation from [1].
+#'
+#' \itemize{
+#'   \item [1] H. Jeffreys (1961). The Theory of Probability (3rd ed.).
+#'     Oxford. p. 432
+#' }
+#' @param bayes_factor Bayes factor to be interpreted
+#' @return a string with the interpretation in English
+#' @export
 interpret_bayes_factor <- function(bayes_factor) {
-  
+
   if (!beautier::is_one_double(bayes_factor)) {
     stop(
       "'bayes_factor' must be one numeric value. \n",
