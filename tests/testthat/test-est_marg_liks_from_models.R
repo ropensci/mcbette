c20f83aac6b2c491f62d7aa4d509c9e0dc5b498b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-15 08:30:59 +0200

    Remove redundant test [skip ci]

--- tests/testthat/test-est_marg_liks_from_models.R
+++ tests/testthat/test-est_marg_liks_from_models.R
@@ -118,10 +118,6 @@ test_that("use, 8 models", {
   expect_true(all(df$weight <= 1.0))
 })
 
-
-
-## TODO
-
 test_that("use, 1 model", {
 
   if (!beastier::is_beast2_installed()) return()
@@ -181,93 +177,6 @@ test_that("use, 1 model, CBS", {
     ),
     "'group_sizes_dimension' .* must be less than the number of taxa"
   )
-})
-
-test_that("use with same RNG seed must result in identical output", {
-
-  if (!beastier::is_on_travis()) return()
-  if (!mauricer::is_beast2_ns_pkg_installed()) return()
-
-  fasta_filename <- system.file("extdata", "simple.fas", package = "mcbette")
-
-  site_models <- list(create_jc69_site_model())
-  clock_models <- list(create_strict_clock_model())
-  # Cannot use a CBS tree prior, as the FASTA file has only two taxa
-  tree_priors <- list(create_yule_tree_prior(), create_bd_tree_prior())
-
-  rng_seed <- 314
-  epsilon <- 1e7
-
-  df_1 <- est_marg_liks(
-    fasta_filename,
-    site_models = site_models,
-    clock_models = clock_models,
-    tree_priors = tree_priors,
-    epsilon = epsilon,
-    rng_seed = rng_seed
-  )
-  df_2 <- est_marg_liks(
-    fasta_filename,
-    site_models = site_models,
-    clock_models = clock_models,
-    tree_priors = tree_priors,
-    epsilon = epsilon,
-    rng_seed = rng_seed
-  )
-  expect_equal(df_1, df_2)
-})
-
-################################################################################
-# OLDSKOOL
-################################################################################
-
-test_that("use", {
-
-  if (!beastier::is_beast2_installed()) return()
-
-  fasta_filename <- system.file("extdata", "simple.fas", package = "mcbette")
-  inference_model_1 <- beautier::create_inference_model(
-    site_model = beautier::create_jc69_site_model(),
-    mcmc = beautier::create_nested_sampling_mcmc()
-  )
-  inference_model_2 <- beautier::create_inference_model(
-    site_model = beautier::create_hky_site_model(),
-    mcmc = beautier::create_nested_sampling_mcmc()
-  )
-  inference_models <- list(inference_model_1, inference_model_2)
-  beast2_options <- beastier::create_beast2_options(
-    beast2_path = beastier::get_default_beast2_bin_path()
-  )
-  beast2_optionses <- list(beast2_options, beast2_options)
-
-  df <- est_marg_liks_from_models(
-    fasta_filename,
-    inference_models = inference_models,
-    beast2_optionses = beast2_optionses,
-    epsilon = 1e7,
-    verbose = TRUE
-  )
-
-  expect_true(is.data.frame(df))
-  expect_true("site_model_name" %in% colnames(df))
-  expect_true("clock_model_name" %in% colnames(df))
-  expect_true("tree_prior_name" %in% colnames(df))
-  expect_true("marg_log_lik" %in% colnames(df))
-  expect_true("marg_log_lik_sd" %in% colnames(df))
-  expect_true("weight" %in% colnames(df))
-
-  expect_true(is.factor(df$site_model_name))
-  expect_true(is.factor(df$clock_model_name))
-  expect_true(is.factor(df$tree_prior_name))
-  expect_true(!is.factor(df$marg_log_lik))
-  expect_true(!is.factor(df$marg_log_lik_sd))
-  expect_true(!is.factor(df$weight))
-
-  expect_true(all(df$weight >= 0.0))
-  expect_true(all(df$weight <= 1.0))
-
-  expect_true(sum(df$marg_log_lik < 0.0, na.rm = TRUE) > 0)
-  expect_true(sum(df$marg_log_lik_sd > 0.0, na.rm = TRUE) > 0)
 })
 
 test_that("use with same RNG seed must result in identical output", {
