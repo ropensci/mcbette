d1255d763cce6296f3a3816a7f1607a8da729246
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-26 18:20:53 +0200

    Moved 'create_test_ns_inference_model' to beautier

--- tests/testthat/test-create_test_ns_inference_model.R
+++
@@ -1,6 +0,0 @@
-test_that("use", {
-
-  inference_model <- create_test_ns_inference_model()
-  expect_true(beautier::is_inference_model(inference_model))
-  expect_true(beautier::is_nested_sampling_mcmc(inference_model$mcmc))
-})
