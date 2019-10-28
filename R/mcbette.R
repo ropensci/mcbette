bdda7b6b58ef8f1d095eceb2c6f6cf457bfacf54
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-28 09:00:34 +0100

    Depend on babette packages, thanks Uwe Ligges

---
+++ R/mcbette.R
@@ -0,0 +1,8 @@
+#' mcbette: Model Comparison Using Babette
+#'
+#' 'mcbette' does a model comparing using `babette`.
+#'
+#' @docType package
+#' @name mcbette
+#' @import beautier tracerer beastier mauricer babette
+NULL
--- tests/testthat/test-onLoad.R
+++
@@ -1,3 +0,0 @@
-test_that("use", {
-  expect_silent(mcbette:::.onLoad())
-})
