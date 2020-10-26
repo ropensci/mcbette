0de5c7e579eab2ea4bf94a0f6d074d529073142c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-26 13:31:45 +0100

    'after' can be NA

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,4 +6,12 @@ before <- get_mcbette_state()
 test_check("mcbette")
 
 # Check if everything is the same again
-expect_identical(before, get_mcbette_state())
+after <- get_mcbette_state()
+if (is.na(before$ns_installed)) {
+  expect_equal(before$beast2_installed, after$beast2_installed)
+  expect_true(is.na(after$ns_installed))
+} else {
+  expect_identical(before, get_mcbette_state())
+}
+
+
