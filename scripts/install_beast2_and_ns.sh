1545c66af52f5ef435623e230bf08b8255a8552c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-05 14:07:09 +0100

    Added some convenient scripts

---
+++ scripts/install_beast2_and_ns.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+Rscript -e 'beastier::install_beast2()'
+Rscript -e 'mauricer::install_beast2_pkg("NS")'
+
---
+++ scripts/uninstall_beast2_and_ns.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+Rscript -e 'mauricer::uninstall_beast2_pkg("NS")'
+Rscript -e 'beastier::uninstall_beast2()'
