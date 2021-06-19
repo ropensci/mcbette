a95cc2280fd88261d2da38f1846da91e36c5756d
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-19 19:42:39 +0200

    Test undeleted files in testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,47 @@
 library(testthat)
 library(mcbette)
 
+
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "tracerer")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "beastier")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "mauricer")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "babette")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "mcbette")))
+)
+
 test_check("mcbette")
+
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "tracerer")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "beastier")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "mauricer")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "babette")))
+)
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "mcbette")))
+)
